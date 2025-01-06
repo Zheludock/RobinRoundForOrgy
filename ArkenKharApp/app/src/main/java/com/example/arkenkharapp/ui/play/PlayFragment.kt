47f1125c083c512de682bc83508547d0a1d5d2df
Zheludock <chepurnykhmv@gmail.com>
2025-01-06 15:42:37 +1000

    first try

--- ArkenKharApp/app/src/main/java/com/example/arkenkharapp/ui/play/PlayFragment.kt
+++ ArkenKharApp/app/src/main/java/com/example/arkenkharapp/ui/play/PlayFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.arkenkharapp.databinding.FragmentNotificationsBinding
+import com.example.arkenkharapp.databinding.FragmentPlayBinding
 
 class PlayFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentPlayBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class PlayFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
+        val playViewModel =
             ViewModelProvider(this).get(PlayViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentPlayBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textPlay
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        playViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
